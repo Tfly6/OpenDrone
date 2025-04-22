9feda92d28b11eb21a99339fc9a60aa4730f71fd
tfly <2085488186@qq.com>
2025-04-22 19:06:46 +0800

    update .gitignore and add shell script

---
+++ shell/record.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rosbag record /mavros/local_position/pose /mavros/local_position/velocity_body /mavros/setpoint_raw/attitude /mavros/local_position/odom /mavros/local_position/accel /mavros/setpoint_position/local /command/trajectory
